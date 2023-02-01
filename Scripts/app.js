c58b4befc34b3088073d9eda612b325d43ac9269
JohanFernandoLema <johan.13fernando@gmail.com>
2023-02-01 14:26:24 -0500

    initial commit

---
+++ Scripts/app.js
@@ -0,0 +1,11 @@
+//IIFE Inmediatly Invoke Function Expression
+
+(function()
+{
+    let Start = () =>
+    {
+        console.log("My App Started!...");
+    }
+    window.addEventListener("load", Start)
+
+})();
