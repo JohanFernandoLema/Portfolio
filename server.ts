3694e4b0715f4634b51b3698ad8aaeda1af54288
JohanFernandoLema <johan.13fernando@gmail.com>
2023-02-17 16:00:56 -0500

    server converted to express

--- server.ts
+++ server.ts
@@ -1,46 +1,16 @@
-import http from 'http';
-import fs from 'fs';
-import mime from 'mime-types';
-
-
-let lookup = mime.lookup;
- 
+import  express from 'express';
+const app = express();
 const port = 3000;
- 
-// when we create a server instance, it is IMMUTABLE = cannot be changed until the server is restarted
 
-const server = http.createServer(function(req, res)
+app.get('/', function(req, res)
 {
-    let path = req.url as string;
+  res.send('Hello World!')
+})
 
-    if(path =="/" || path =="/home")
-    {
-        path = "/index.html";
-    }
-
-    let mime_type= lookup(path.substring(1))as string;
-    console.log(`Mime Type: ${mime_type}`);
-
-    fs.readFile(__dirname + path, function(err, data)
-    {
-         if(err)
-         {
-            res.writeHead(404); // status - file not found
-            return res.end("ERROR: 404 - File Not Found"); // output the error message to the page
-        }
-        res.setHeader("X-Content-Type-Options", "nosniff"); // security guard
-        res.writeHead(200, {'Content-Type': mime_type});
-        return res.end(data);
-    });
-});
-
-
-server.listen(port, function()
+app.listen(port, function()
 {
-    console.log(`Server running at port: ${port}`)
-});
-
-
+  console.log(`Example app listening on port ${port}`);
+})
 
 
 
