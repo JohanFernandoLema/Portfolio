2b099665b595f614413e9493f7d431df136e674f
JohanFernandoLema <johan.13fernando@gmail.com>
2023-02-22 18:00:04 -0500

    basic routes added

--- Server/Routes/index.ts
+++ Server/Routes/index.ts
@@ -3,7 +3,33 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req: express.Request, res: express.Response, next: express.NextFunction) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Johan Lema Portfolio', page: 'home' });
 });
+
+/* GET home page. */
+router.get('/home', function(req: express.Request, res: express.Response, next: express.NextFunction) {
+  res.render('index', { title: 'Home', page: 'home' });
+});
+
+/* GET About page. */
+router.get('/about', function(req: express.Request, res: express.Response, next: express.NextFunction) {
+  res.render('index', { title: 'About Me', page: 'about' });
+});
+
+/* GET Projects page. */
+router.get('/projects', function(req: express.Request, res: express.Response, next: express.NextFunction) {
+  res.render('index', { title: 'My Projects', page: 'projects' });
+});
+
+/* GET Services page. */
+router.get('/services', function(req: express.Request, res: express.Response, next: express.NextFunction) {
+  res.render('index', { title: 'Services', page: 'services' });
+});
+
+/* GET Contact page. */
+router.get('/contact', function(req: express.Request, res: express.Response, next: express.NextFunction) {
+  res.render('index', { title: 'Contact', page: 'contact' });
+});
+
 
 export default router;
