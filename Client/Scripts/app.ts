70cb8de6cb2728d9a8a56a6abd331ecf680f8e60
JohanFernandoLema <johan.13fernando@gmail.com>
2023-04-06 16:51:31 -0400

    navigation bar added

--- Client/Scripts/app.ts
+++ Client/Scripts/app.ts
@@ -29,3 +29,20 @@
     }
     window.addEventListener("load", Start);
 })();
+
+const primaryNav = document.querySelector('.primary-navigation') as HTMLButtonElement;
+const navToggle = document.querySelector(".mobile-nav-toggle") as HTMLButtonElement;
+navToggle.addEventListener('click', function () {
+    const visibility = primaryNav.getAttribute('data-visible');
+    if(visibility === "false")
+    {
+        primaryNav.setAttribute("data-visible", "true");
+        navToggle.setAttribute("aria-expanded", "true");
+    }else if(visibility === "true") 
+    {
+        primaryNav.setAttribute("data-visible", "false");
+        navToggle.setAttribute("aria-expanded", "true");
+    }
+});
+
+
